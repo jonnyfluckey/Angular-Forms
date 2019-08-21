e69f3c4a2d422c111f5a7ac4c2f4d7eb861c5754
Jonny Fluckey <jonathon.fluckey@gmail.com>
2019-08-20 21:57:18 -0600

    updated form, added css, and functions

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -23,4 +24,11 @@ export class UserSettingsFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    console.log('in onSubmit: ', form.valid);
+  }
+
+  onBlur(field : NgModel) {
+    console.log('in onBlur: ', field.valid);
+  }
 }
